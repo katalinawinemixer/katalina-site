0884c95e974e9c1d075f540bf8148c658a58a36c
Katalina Londoño <katalinawinemixer@users.noreply.github.com>
2026-04-28 15:52:23 -0700

    Add Safari favicon fallback

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,7 +48,9 @@ export const metadata: Metadata = {
   creator: siteConfig.author,
   publisher: siteConfig.author,
   icons: {
+    shortcut: [{ url: "/favicon.ico" }],
     icon: [
+      { url: "/favicon.ico", sizes: "any" },
       { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
       { url: "/icon-192.png", sizes: "192x192", type: "image/png" },
     ],
